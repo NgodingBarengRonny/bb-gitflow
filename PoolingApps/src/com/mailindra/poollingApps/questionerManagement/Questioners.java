009f1ce3636be4d8ccc250a4e66f377da0b95a41
Ronny Mailindra <rmailindra@gmail.com>
2021-06-27 18:07:58 +0700

    add file test

---
+++ PoolingApps/src/com/mailindra/poollingApps/questionerManagement/Questioners.java
@@ -0,0 +1,24 @@
+package com.mailindra.poollingApps.questionerManagement;
+public class Questioners {
+    String question;
+    String id;
+
+    public Questioners() {
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
